c99f597798dba463802468f3819580ceec67065c
logicp <logicp@logicp.ca>
2022-02-03 22:25:41 -0500

    cleanup

--- src/util.hpp
+++ src/util.hpp
@@ -2,6 +2,15 @@
 
 #include <sstream>
 #include <fstream>
+#include <iostream>
 
 std::string ReadFile (const std::string& path);
 void        SaveToken(const std::string& token, const std::string& name);
+
+template<typename... Args>
+static void log(Args... args)
+{
+  for (const auto& s : {args...})
+    std::cout << s;
+  std::cout << std::endl;
+}
